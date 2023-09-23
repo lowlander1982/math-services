54f68ca9755b66f353b757551fd43441f6b1fcfd
Juan Manuel Valencia <jmvalenciz@protonmail.com>
2023-09-23 07:58:43 -0500

    Update complex_functions.go

--- controllers/complex_expresions/complex_functions.go
+++ controllers/complex_expresions/complex_functions.go
@@ -9,6 +9,12 @@ import (
 func GetComplexFunctions() map[string]govaluate.ExpressionFunction {
 	return map[string]govaluate.ExpressionFunction{
 		"sin": sin,
+		"cos": cos,
+		"tan": tan,
+		"sqrt": sqrt,
+		"abs": abs,
+		"exp": exp,
+		"log": log,
 	}
 
 }
@@ -16,3 +22,23 @@ func GetComplexFunctions() map[string]govaluate.ExpressionFunction {
 func sin(args ...interface{}) (interface{}, error) {
 	return math.Sin(args[0].(float64)), nil
 }
+
+func cos(args ...interface{}) (interface{}, error) {
+	return math.Cos(args[0].(float64)), nil
+}
+func sqrt(args ...interface{}) (interface{}, error) {
+	return math.Sqrt(args[0].(float64)), nil
+}
+func abs(args ...interface{}) (interface{}, error) {
+	return math.Abs(args[0].(float64)), nil
+}
+func exp(args ...interface{}) (interface{}, error) {
+	return math.Exp(args[0].(float64)), nil
+}
+func log(args ...interface{}) (interface{}, error) {
+	return math.Log(args[0].(float64)), nil
+}
+func tan(args ...interface{}) (interface{}, error) {
+	return math.Tan(args[0].(float64)), nil
+}
+
